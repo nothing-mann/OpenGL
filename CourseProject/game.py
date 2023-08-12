8e8452e7ae1e2d23042c43e8135967a860ffd1de
nothing-mann <i.sudeep.subedi@gmail.com>
2023-08-12 22:29:06 +0545

    Loaded an .obj model to render the rotating cube.

--- CourseProject/game.py
+++ CourseProject/game.py
@@ -5,6 +5,120 @@ import ctypes
 from OpenGL.GL.shaders import compileProgram, compileShader
 import pyrr
 
+
+def loadMesh(filename: str) -> list[float]:
+    """
+        Load a mesh from an obj file.
+
+        Parameters:
+
+            filename: the filename.
+
+        Returns:
+
+            The loaded data, in a flattened format.
+    """
+
+    v = []
+    vt = []
+    vn = []
+
+    vertices = []
+
+    with open(filename, "r") as file:
+
+        line = file.readline()
+
+        while line:
+
+            words = line.split(" ")
+            match words[0]:
+
+                case "v":
+                    v.append(read_vertex_data(words))
+
+                case "vt":
+                    vt.append(read_texcoord_data(words))
+
+                case "vn":
+                    vn.append(read_normal_data(words))
+
+                case "f":
+                    read_face_data(words, v, vt, vn, vertices)
+
+            line = file.readline()
+
+    return vertices
+
+
+def read_vertex_data(words: list[str]) -> list[float]:
+    """
+        Returns a vertex description.
+    """
+
+    return [
+        float(words[1]),
+        float(words[2]),
+        float(words[3])
+    ]
+
+
+def read_texcoord_data(words: list[str]) -> list[float]:
+    """
+        Returns a texture coordinate description.
+    """
+
+    return [
+        float(words[1]),
+        float(words[2])
+    ]
+
+
+def read_normal_data(words: list[str]) -> list[float]:
+    """
+        Returns a normal vector description.
+    """
+
+    return [
+        float(words[1]),
+        float(words[2]),
+        float(words[3])
+    ]
+
+
+def read_face_data(
+        words: list[str],
+        v: list[list[float]], vt: list[list[float]],
+        vn: list[list[float]], vertices: list[float]) -> None:
+    """
+        Reads an edgetable and makes a face from it.
+    """
+
+    triangleCount = len(words) - 3
+
+    for i in range(triangleCount):
+        make_corner(words[1], v, vt, vn, vertices)
+        make_corner(words[2 + i], v, vt, vn, vertices)
+        make_corner(words[3 + i], v, vt, vn, vertices)
+
+
+def make_corner(corner_description: str,
+                v: list[list[float]], vt: list[list[float]],
+                vn: list[list[float]], vertices: list[float]) -> None:
+    """
+        Composes a flattened description of a vertex.
+    """
+
+    v_vt_vn = corner_description.split("/")
+
+    for element in v[int(v_vt_vn[0]) - 1]:
+        vertices.append(element)
+    for element in vt[int(v_vt_vn[1]) - 1]:
+        vertices.append(element)
+    for element in vn[int(v_vt_vn[2]) - 1]:
+        vertices.append(element)
+
+
 class Cube:
     def __init__(self, position, eulers):
         self.position = np.array(position, dtype=np.float32)
@@ -26,8 +140,8 @@ class App:
             eulers=[0, 0, 0]
         )
 
-        self.cube_mesh = CubeMesh()
-        self.random_texture = Material("me.jpeg")
+        self.cube_mesh = Mesh("models/cube.obj")
+        self.random_texture = Material("textures/queen.png")
 
         projection_transform = pyrr.matrix44.create_perspective_projection(
             fovy=45, aspect=640 / 480,
@@ -104,77 +218,137 @@ class App:
         glDeleteProgram(self.shader)
         pg.quit()
 
-class CubeMesh:
-    # x,y,z,s,t
-    def __init__(self):
-        self. vertices = (
-            -0.5, -0.5, -0.5, 0, 0,
-             0.5, -0.5, -0.5, 1, 0,
-             0.5,  0.5, -0.5, 1, 1,
 
-             0.5,  0.5, -0.5, 1, 1,
-            -0.5,  0.5, -0.5, 0, 1,
-            -0.5, -0.5, -0.5, 0, 0,
+class Mesh:
+    """
+        A mesh that can represent an obj model.
+    """
 
-            -0.5, -0.5,  0.5, 0, 0,
-             0.5, -0.5,  0.5, 1, 0,
-             0.5,  0.5,  0.5, 1, 1,
+    def __init__(self, filename: str):
+        """
+            Initialize the mesh.
+        """
 
-             0.5,  0.5,  0.5, 1, 1,
-            -0.5,  0.5,  0.5, 0, 1,
-            -0.5, -0.5,  0.5, 0, 0,
+        # x, y, z, s, t, nx, ny, nz
+        vertices = loadMesh(filename)
+        self.vertex_count = len(vertices) // 8
+        vertices = np.array(vertices, dtype=np.float32)
 
-            -0.5,  0.5,  0.5, 1, 0,
-            -0.5,  0.5, -0.5, 1, 1,
-            -0.5, -0.5, -0.5, 0, 1,
-
-            -0.5, -0.5, -0.5, 0, 1,
-            -0.5, -0.5,  0.5, 0, 0,
-            -0.5,  0.5,  0.5, 1, 0,
-
-             0.5,  0.5,  0.5, 1, 0,
-             0.5,  0.5, -0.5, 1, 1,
-             0.5, -0.5, -0.5, 0, 1,
-
-             0.5, -0.5, -0.5, 0, 1,
-             0.5, -0.5,  0.5, 0, 0,
-             0.5,  0.5,  0.5, 1, 0,
-
-            -0.5, -0.5, -0.5, 0, 1,
-             0.5, -0.5, -0.5, 1, 1,
-             0.5, -0.5,  0.5, 1, 0,
-
-             0.5, -0.5,  0.5, 1, 0,
-            -0.5, -0.5,  0.5, 0, 0,
-            -0.5, -0.5, -0.5, 0, 1,
-
-            -0.5,  0.5, -0.5, 0, 1,
-             0.5,  0.5, -0.5, 1, 1,
-             0.5,  0.5,  0.5, 1, 0,
-
-             0.5,  0.5,  0.5, 1, 0,
-            -0.5,  0.5,  0.5, 0, 0,
-            -0.5,  0.5, -0.5, 0, 1
-        )
-        self.vertex_count = len(self.vertices) // 5
-        #we use np.array to convert the vertices into float32 datatype for opengl to read the vertices properly.
-        self.vertices = np.array(self.vertices, dtype=np.float32)
-
-        self.vao = glGenVertexArrays(1) #Generates vertex arrays
+        self.vao = glGenVertexArrays(1)
         glBindVertexArray(self.vao)
-        self.vbo = glGenBuffers(1) #Generates vertex buffers
-        glBindBuffer(GL_ARRAY_BUFFER,self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
 
+        # Vertices
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+        # position
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(0))
-
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
+        # texture
         glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1 , 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
 
-    def destroy(self):
+    def arm_for_drawing(self) -> None:
+        """
+            Arm the triangle for drawing.
+        """
+        glBindVertexArray(self.vao)
+
+    def draw(self) -> None:
+        """
+            Draw the triangle.
+        """
+
+        glDrawArrays(GL_TRIANGLES, 0, self.vertex_count)
+
+    def destroy(self) -> None:
+        """
+            Free any allocated memory.
+        """
+
         glDeleteVertexArrays(1, (self.vao,))
         glDeleteBuffers(1, (self.vbo,))
+
+    def destroy(self) -> None:
+        """
+            Free any allocated memory.
+        """
+
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1, (self.vbo,))
+
+
+# class CubeMesh:
+#     # x,y,z,s,t
+#     def __init__(self):
+#         self. vertices = (
+#             -0.5, -0.5, -0.5, 0, 0,
+#              0.5, -0.5, -0.5, 1, 0,
+#              0.5,  0.5, -0.5, 1, 1,
+#
+#              0.5,  0.5, -0.5, 1, 1,
+#             -0.5,  0.5, -0.5, 0, 1,
+#             -0.5, -0.5, -0.5, 0, 0,
+#
+#             -0.5, -0.5,  0.5, 0, 0,
+#              0.5, -0.5,  0.5, 1, 0,
+#              0.5,  0.5,  0.5, 1, 1,
+#
+#              0.5,  0.5,  0.5, 1, 1,
+#             -0.5,  0.5,  0.5, 0, 1,
+#             -0.5, -0.5,  0.5, 0, 0,
+#
+#             -0.5,  0.5,  0.5, 1, 0,
+#             -0.5,  0.5, -0.5, 1, 1,
+#             -0.5, -0.5, -0.5, 0, 1,
+#
+#             -0.5, -0.5, -0.5, 0, 1,
+#             -0.5, -0.5,  0.5, 0, 0,
+#             -0.5,  0.5,  0.5, 1, 0,
+#
+#              0.5,  0.5,  0.5, 1, 0,
+#              0.5,  0.5, -0.5, 1, 1,
+#              0.5, -0.5, -0.5, 0, 1,
+#
+#              0.5, -0.5, -0.5, 0, 1,
+#              0.5, -0.5,  0.5, 0, 0,
+#              0.5,  0.5,  0.5, 1, 0,
+#
+#             -0.5, -0.5, -0.5, 0, 1,
+#              0.5, -0.5, -0.5, 1, 1,
+#              0.5, -0.5,  0.5, 1, 0,
+#
+#              0.5, -0.5,  0.5, 1, 0,
+#             -0.5, -0.5,  0.5, 0, 0,
+#             -0.5, -0.5, -0.5, 0, 1,
+#
+#             -0.5,  0.5, -0.5, 0, 1,
+#              0.5,  0.5, -0.5, 1, 1,
+#              0.5,  0.5,  0.5, 1, 0,
+#
+#              0.5,  0.5,  0.5, 1, 0,
+#             -0.5,  0.5,  0.5, 0, 0,
+#             -0.5,  0.5, -0.5, 0, 1
+#         )
+#         self.vertex_count = len(self.vertices) // 5
+#         #we use np.array to convert the vertices into float32 datatype for opengl to read the vertices properly.
+#         self.vertices = np.array(self.vertices, dtype=np.float32)
+#
+#         self.vao = glGenVertexArrays(1) #Generates vertex arrays
+#         glBindVertexArray(self.vao)
+#         self.vbo = glGenBuffers(1) #Generates vertex buffers
+#         glBindBuffer(GL_ARRAY_BUFFER,self.vbo)
+#         glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+#
+#         glEnableVertexAttribArray(0)
+#         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(0))
+#
+#         glEnableVertexAttribArray(1)
+#         glVertexAttribPointer(1 , 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
+#
+#     def destroy(self):
+#         glDeleteVertexArrays(1, (self.vao,))
+#         glDeleteBuffers(1, (self.vbo,))
 
 class Material:
     def __init__(self, filepath: str):
